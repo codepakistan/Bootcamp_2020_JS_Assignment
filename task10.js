e1e41bcad10cb4aefe0f4c3396ff52df0c082f99
Ali Nauman Sandhu <bc210209266@vu.edu.pk>
2022-06-15 18:25:56 +0500

    Task 9 and Task 10 of Assignment 01 JS

---
+++ task10.js
@@ -0,0 +1,22 @@
+var a;
+document.write(`Value after variable declaration is : ${a} `);
+
+a = 5;
+
+document.write(`<br>Initial value : ${a}`);
+
+var a = ++a;
+
+document.write(`<br>Value after increment is : ${a}`);
+
+var a = a+7;
+
+document.write(`<br>Value after addition is : ${a}`);
+
+var a= --a;
+
+document.write(`<br>Value after decrement is : ${a}`)
+
+var a = a%3;
+
+document.write(`<br>The remainder is : ${a}`)
