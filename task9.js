5368c65911b22b573b1e2862ac2f085465650c97
Ali Nauman Sandhu <bc210209266@vu.edu.pk>
2022-06-15 17:15:12 +0500

    Task 8 and Task 9 of Assignemnt 01 JS

---
+++ task9.js
@@ -0,0 +1,14 @@
+var a = Number(prompt("Enter number 1 = "));
+var b = Number(prompt("Enter number 2 = "));
+
+var diff = a - b;
+document.write(`Difference of ${a} and ${b} is ${diff}`);
+
+var product = a * b;
+document.write(`<br>Product of ${a} and ${b} is ${product}</br>`);
+
+var quotient = a / b;
+document.write(`Quotient of ${a} and ${b} is ${quotient}</br>`);
+
+var modulus = a % b;
+document.write(`Modulus of ${a} and ${b} is ${modulus}</br>`);
